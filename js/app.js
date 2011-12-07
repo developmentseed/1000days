35a7951771a0c82a3c15dde04960dfa47b5a16e0
tristen <tristen@tristen.ca>
2011-12-07 12:07:52 -0500

    more cleanup :)

--- js/app.js
+++ js/app.js
@@ -1,158 +1,35 @@
-var m,
-	legend,
-    mm = com.modestmaps,
-    activeLayer = 'djohnson.stunting_orange',
-    activeData = 'djohnson.country_points2',
-    activeData2= 'djohnson.admin_points',
-    borders = 'mapbox.world-borders-light',
-    baselayer ='mapbox.world-blank-bright',
+var m;
+var legend;
+var mm = com.modestmaps;
+var activeLayer = 'djohnson.stunting_orange';
+var activeData = 'djohnson.country_points2';
+var activeData2= 'djohnson.admin_points';
+var borders = 'mapbox.world-borders-light';
+var baselayer ='mapbox.world-blank-bright';
 
-   
- 
- layers = [baselayer, activeLayer, borders,activeData, activeData2
-        ].join(','),
-  	    url = 'http://a.tiles.mapbox.com/v1/' + layers + '.jsonp';
+// TODO: Change this
+$(document).ready(function () {
+    $('a.share').click(function(e){
+        e.preventDefault();
+        $('#small-drawer .drawer-content').html(MA.template.share());
+        $('#small-drawer, #overlay').addClass('active');
 
-		function getTiles() { 
-		  return [
-		            "http://a.tiles.mapbox.com/v1/"+layers+"/{z}/{x}/{y}.jpg",
-		            "http://b.tiles.mapbox.com/v1/"+layers+"/{z}/{x}/{y}.jpg",
-		            "http://c.tiles.mapbox.com/v1/"+layers+"/{z}/{x}/{y}.jpg",
-		            "http://d.tiles.mapbox.com/v1/"+layers+"/{z}/{x}/{y}.jpg"
-		         ]
-		};
+        var shareContent = $('.share-content');
+        var twitter = 'http://twitter.com/intent/tweet?status=' +
+        'MobileActive Mobile Data ' + encodeURIComponent(window.location);
+        var facebook = 'https://www.facebook.com/sharer.php?t=MobileActive%20Mobile%20Data%20Data&u=' +
+        encodeURIComponent(window.location);
 
-		function getGrids() { 
-		  return [
-		        "http://a.tiles.mapbox.com/v1/"+layers+"/{z}/{x}/{y}.grid.json",
-	            "http://b.tiles.mapbox.com/v1/"+layers+"/{z}/{x}/{y}.grid.json",
-	            "http://c.tiles.mapbox.com/v1/"+layers+"/{z}/{x}/{y}.grid.json",
-	            "http://d.tiles.mapbox.com/v1/"+layers+"/{z}/{x}/{y}.grid.json"
-		         ]
-		};
+        document.getElementById('twitter').href = twitter;
+        document.getElementById('facebook').href = facebook;
 
-	function refreshMap() {
-	 url = 'http://a.tiles.mapbox.com/v1/' + layers + '.jsonp';
-  		wax.tilejson(url, function(tilejson) {
-	        
-  			tilejson.minzoom = 2;
-	      	tilejson.maxzoom = 7;
-	      	tilejson.tiles = getTiles();
-	      	tilejson.grids = getGrids();
-	      	m.setProvider(new wax.mm.connector(tilejson));file:///Users/howardthethird/Desktop/javascript%20test/thousand_days/index_new.html#
-		  	 $('.wax-legends').remove(); 
-			    legend = wax.mm.legend(m, tilejson).appendTo(m.parent);
-			    interaction.remove();
-			    interaction = wax.mm.interaction(m, tilejson);
-			});
-		}
-	$(function (){
-		//build map
-		$('baselayer').addClass('active');
-	    wax.tilejson(url, function(tilejson) {
-	      tilejson.minzoom = 2;
-	      tilejson.maxzoom = 7;
-	      tilejson.tiles = getTiles();
-	      tilejson.grids = getGrids();
-	      
-	      m = new mm.Map('map',
-	        new wax.mm.connector(tilejson),
-	        null,
-	        [
-	          new mm.DragHandler,
-	          new mm.DoubleClickHandler,
-	          new mm.TouchHandler
-	        ]);
-	      interaction = wax.mm.interaction(m, tilejson);
-	      wax.mm.zoomer(m, tilejson).appendTo(m.parent);
-	      legend = wax.mm.legend(m, tilejson).appendTo(m.parent);
-	      wax.mm.zoombox(m, tilejson);
-	      
-	      m.setCenterZoom(new mm.Location( 5
-		, 15), 3);
-	      
-	      // Bandwidth detection control and switch element
-		  var detector = wax.mm.bwdetect(m, {
-		    auto: true,
-		    png: '.png32?'
-		  });
-		  m.addCallback('drawn', function lqDetect(modestmap, e) {
-		    if (!detector.bw()) {
-		      $('#bwtoggle').removeClass('active');
-		    }
-		    m.removeCallback(lqDetect);
-		  });
-		  $('a#bwtoggle').click(function(e) {
-		      e.preventDefault();
-		      $(this).hasClass('active') ? $(this).removeClass('active') : $(this).addClass('active');
-		      detector.bw(!detector.bw());
-		  });
-	      
-	    });
-	    
-	    //contextual layer switching
-	    $('.layers li a').click(function() {
-	    	activeLayer = this.id;
-	    	$('.layers li a').removeClass('active');
-      		$(this).addClass('active');
-	    	layers = [baselayer, activeLayer, borders,activeData, activeData2
-			        ].join(','),
-			  	    url = 'http://a.tiles.mapbox.com/v1/' + layers + '.jsonp';
-            
-            refreshMap();
-        });
-        
-        //point data selector
-        $('#data-select').change(function() {
-        	activeLayer = $('#data-select option:selected')[0].id;
-        	layers = [baselayer, activeLayer, borders,activeData, activeData2
-			        ].join(','),
-            
-            refreshMap();
-        });
-	});
-	
-	// Update and show embed script
-	$(function (){
-	  $('a.embed').click(function (e) {
-	    e.preventDefault();
-	
-	    var splitLayers = layers.split(','),
-	        embedlayers = '',
-	        center = m.pointLocation(new mm.Point(m.dimensions.x/2,m.dimensions.y/2));
-	
-	    $.each(splitLayers, function(num, key) {
-	      embedlayers += '&amp;layers%5B%5D=' + key;
-	    });
-	
-	    var embedId = 'ts-embed-' + (+new Date());
-	    var url = '&amp;size=700'
-	            + '&amp;size%5B%5D=550'
-	            + '&amp;center%5B%5D=' + center.lon
-	            + '&amp;center%5B%5D=' + center.lat
-	            + '&amp;center%5B%5D=' + 6
-	            + embedlayers
-	            + '&amp;options%5B%5D=zoomwheel'
-	            + '&amp;options%5B%5D=legend'
-	            + '&amp;options%5B%5D=tooltips'
-	            + '&amp;options%5B%5D=zoombox'
-	            + '&amp;options%5B%5D=zoompan'
-	            + '&amp;options%5B%5D=attribution'
-	            + '&amp;el=' + embedId;
-	
-	    $('.tip input').attr('value', "<div id='"
-	      + embedId
-	      + "-script'><script src='http://tiles.mapbox.com/mix/api/v1/embed.js?api=mm"
-	      + url
-	      + "'></script></div>");
-	
-	    if ($('#embed').hasClass('active')) {
-	      $('#embed').removeClass('active');
-	    } else {
-	      $('#embed').addClass('active');
-	      $('#embed-code')[0].tabindex = 0;
-	      $('#embed-code')[0].focus();
-	      $('#embed-code')[0].select();
-	    }
-	  });
-	});
+        var center = m.pointLocation(new mm.Point(m.dimensions.x/2,m.dimensions.y/2));
+        var embedUrl = 'http://api.tiles.mapbox.com/v2/' + layers + '/mm/zoompan,tooltips,legend,bwdetect.html#' + m.coordinate.zoom +
+                        '/' + center.lat + '/' + center.lon;
+        $('#embed-code-field textarea').attr('value', '<iframe src="' + embedUrl +
+            '" frameborder="0" width="650" height="500"></iframe>');
+
+        $('#embed-code')[0].tabindex = 0;
+        $('#embed-code')[0].select();
+    });
+});
